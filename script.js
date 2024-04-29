cac11c9d8b81b203dd1ba28190c50fa8697220d4
dition24 <117057559+dition24@users.noreply.github.com>
2024-04-29 18:18:19 -0400

    sets base for deployment

--- script.js
+++ script.js
@@ -1,43 +1,6 @@
 const form = document.getElementById('form');
-const result = document.getElementById('result');
-
-require("dotenv").config();
-const ACCESS_KEY = process.env.ACCESS_KEY;
             
-form.addEventListener('submit', function(e) {
-    const formData = new FormData(form);
-    e.preventDefault();
-            
-    const object = Object.fromEntries(formData);
-    const json = JSON.stringify(object);
-            
-    result.innerHTML = "Please wait..."
-            
-    fetch('https://api.web3forms.com/submit', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-            },
-        body: json
-    })
-    .then(async (response) => {
-        let json = await response.json();
-        if (response.status == 200) {
-            result.innerHTML = json.message;
-        } else {
-            console.log(response);
-            result.innerHTML = json.message;
-        }
-    })
-    .catch(error => {
-        console.log(error);
-        result.innerHTML = "Something went wrong!";
-    })
-    .then(function() {
-        form.reset();
-        setTimeout(() => {
-            result.style.display = "none";
-        }, 3000);
-    });
-});
+window.onload = function() {
+    // Reset the form fields when the page loads
+    document.getElementById("form").reset();
+};
